39a2bc7d68577cdd1d98bca15f40b2fda8d218de
Andres Sancho <andrem.san12@gmail.com>
2018-03-31 21:03:37 -0600

    Pantalla Traductor de LInks

--- app/src/main/java/com/example/alexandramolina/cely/MainActivity.java
+++ app/src/main/java/com/example/alexandramolina/cely/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void abrirActivityRegistrase(){
-        Intent intent = new Intent(this, RegistrarseActivity.class);
+        Intent intent = new Intent(this, Transalation.class);
         startActivity(intent);
     }
 }
