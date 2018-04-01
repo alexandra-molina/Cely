9ba86406b36301d5e5cbd6103a219882a2a762cd
Alexandra <alemoli9715@gmail.com>
2018-03-31 22:24:45 -0600

    Avance traductor

--- app/src/main/java/com/example/alexandramolina/cely/MainActivity.java
+++ app/src/main/java/com/example/alexandramolina/cely/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txt_ayuda;
     private TextView txt_registarse;
+    private Button btn_iniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         txt_ayuda = findViewById(R.id.txt_ayuda);
         txt_registarse = findViewById(R.id.txt_registrarse);
+        btn_iniciarSesion = findViewById(R.id.btn_iniciarSesion);
 
         txt_ayuda.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 abrirActivityRegistrase();
             }
         });
+        btn_iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirActivityTraductor();
+            }
+        });
     }
     public void abrirActivityAyuda(){
         Intent intent = new Intent(this, AyudaActivity.class);
         startActivity(intent);
     }
     public void abrirActivityRegistrase(){
+        Intent intent = new Intent(this, RegistrarseActivity.class);
+        startActivity(intent);
+    }
+    public void abrirActivityTraductor(){
         Intent intent = new Intent(this, Translation.class);
         startActivity(intent);
     }
