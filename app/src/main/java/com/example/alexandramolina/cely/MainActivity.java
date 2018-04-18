e805ce2800b5e35a4686f5a9cc553029a92373fd
Andres Sancho <andrem.san12@gmail.com>
2018-04-18 09:58:23 -0600

    Pruebas requests

--- app/src/main/java/com/example/alexandramolina/cely/MainActivity.java
+++ app/src/main/java/com/example/alexandramolina/cely/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.alexandramolina.cely;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -56,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void abrirActivityRegistrase(){
+        registro();
         Intent intent = new Intent(this, RegistrarseActivity.class);
         startActivity(intent);
     }
@@ -69,7 +84,62 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void abrirActivityBienvenido(){
+        iniciarSesion();
         Intent intent = new Intent(this, BienvenidoActivity.class);
         startActivity(intent);
     }
+
+    public void iniciarSesion(){
+
+        StringRequest ingredienteRequest = new StringRequest(Request.Method.POST, "https://celytranslator.herokuapp.com/v1/sessions", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Pruebaaaa",response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email","andrem.san12@gmail.com");
+                params.put("password","123475");
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(ingredienteRequest);
+
+    }
+    public void registro(){
+
+        StringRequest ingredienteRequest = new StringRequest(Request.Method.POST, "https://celytranslator.herokuapp.com/v1/registrations", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Pruebaaaa",response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email","andrem.san12@gmail.com");
+                params.put("password","1234756");
+                params.put("name","La trucha");
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(ingredienteRequest);
+
+    }
+
+
 }
